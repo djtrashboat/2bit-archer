b935ecca6e4b4eb9ac3575b8645669b8d8c7ec66
djtrashboat <enricommandelli@gmail.com>
2020-07-11 17:06:46 -0300

    map limit

--- Source/drawline.gd
+++ Source/drawline.gd
@@ -7,13 +7,15 @@ func _ready():
 	set_process(false)
 
 func _ready_to_draw():
-	global_position_i = get_global_mouse_position()
+	#global_position_i = get_global_mouse_position()
+	global_position_i =  get_local_mouse_position()
 	#mouse_position_i = get_viewport().get_mouse_position()
 	set_process(true)
 	
 
 func _draw():
-	draw_line(global_position_i, get_global_mouse_position(), Color(33, 125, 99), 1.5)
+	#draw_line(global_position_i, get_global_mouse_position(), Color(33, 125, 99), 1.5)
+	draw_line(global_position_i, get_local_mouse_position(), Color(33, 125, 99), 1.5)
 
 func _process(delta):
 	update()
