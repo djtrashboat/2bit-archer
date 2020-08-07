f759f2b062c32baa9cd75f02b13ddbbb7fdccb42
Mudrost <mvccogo@gmail.com>
2020-08-06 23:53:33 -0300

    Added base score for coin

--- Source/powerups/coin.gd
+++ Source/powerups/coin.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-
+export var base_Coin_score: = 100 #base score for coins
 
 func _on_pickup_coin_area_entered(area: Area2D) -> void:
 	if area.name == "pickuper":
+		PlayerData.score += base_Coin_score
 		queue_free()
