f60c30d230a356c4fd7203b6994ef104c9d057f1
djtrashboat <enricommandelli@gmail.com>
2020-08-08 01:57:44 -0300

    limpando o codigo

--- Source/Actors/Player/Player.gd
+++ Source/Actors/Player/Player.gd
@@ -19,6 +19,35 @@ var extra_jump: bool = false#quando o player tem asas, ele recebe um pulo extra
 var can_shoot: bool = true#determina se o player pode atirar
 var imune: bool = false#true se o player estiver imune
 
+func _on_EnemyDetector_body_entered(body: Node) -> void:#quando o detector de inimigo intersecta com um corpo (marcado como inimigo)
+	if !imune:#se nao estiver estiver imune
+		take_damage()
+		imune = true#se ele ainda nao morreu, depois de levar dano, torna-se imune por um tempo
+		$dmg_delay.start()#comeca o timer pra deixar de ser imune
+
+#pick power ups and interact with some areas
+func _on_pickuper_area_entered(area: Area2D) -> void:
+	if area.name == "pickup_shield":#se entrar na area do shield, pega o shield
+		pickup_shield()
+	if area.name == "pickup_wing":#se entrar na area da wing, pega a wing
+		pickup_wing()
+	if area.name == "sticky_wall_area":#se entrar na area da wall, ele gruda e vai caindo devagar
+		stick_to_wall()# gravitytemp eh uma var temporaria, assim que ele sai dessa area gravitytemp recebe gravity (const)
+
+#saindo da area (sticky_wall) a gravidade volta ao normal
+func _on_pickuper_area_exited(area: Area2D) -> void:#saindo da area (sticky_wall) a gravidade volta ao normal
+	gravitytemp = gravity
+
+#no fim do time, o player pode atirar novamente
+func _on_arrow_delay_timeout() -> void:
+	can_shoot = true
+
+#no fim da invencibilidade temporaria, o player pode levar dano novamente
+func _on_dmg_delay_timeout() -> void:
+	imune = false
+
+
+#*******************************************
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravitytemp * delta#aplicando a gravidade
 	if $ceilingcast.is_colliding():#detecta se o player esta no teto
@@ -26,14 +55,25 @@ func _physics_process(delta: float) -> void:
 	elif is_on_wall():#bounce bounce (on walls)
 		_velocity.x *= -0.3#coeficiente de bounce
 		enable_mov = true#quando o player bate na parede, ele ganha o comando de volta
-		if is_winged:
+		if is_winged:#se ele estiver alado, ele tambem ganha seu pulo extra de volta
 			extra_jump = true
 	if enable_mov:#os comandos so funcionam se enablemov for true
 		_get_input()
 	move_and_slide(_velocity * _speed)#vrummmm
 	#if can_shoot:
-	_get_arrow_strength()#chama o input da flecha
+	#_get_arrow_strength()#chama o input da flecha
+#*******************************************
 
+#chamado todo frame
+func _update():
+	if !mousehold:#se o player nao esta segurando o botao do mouse
+		play_animation(_velocity)#a animacao toca de acordo com a velocidade do player
+		shake_stop()#a tela nao treme
+	else:#se o player esta segurando o botao do mouse
+		shake_screen()#a tela treme
+		$AnimatedSprite.play("populo")#a animacao populo toca
+	$bownarrow.rotation = (arrow_position_i - get_viewport().get_mouse_position()).angle()#roda o arco pro lugar certo
+	_get_arrow_strength()
 
 func _get_input():
 	if Input.is_action_just_pressed("click_right"):#guarda a posicao do clique do mouse
@@ -49,42 +89,44 @@ func _get_input():
 		else:
 			enable_mov = false#assim que o player aplica o impulso, ele perde o controle do personagem
 		mousehold = false#quando solta o botao, mouse hold vira falso
-		#populo = false#o player nao esta mais se preparando para o pulo (pois ja pulou)
 
-
-func _get_arrow_strength():#pega o input do mouse_left e guarda as variaveis
+#pega o input do mouse_left e guarda as variaveis
+func _get_arrow_strength():
 	if Input.is_action_just_pressed("click_left"):#guarda a posicao do clique do mouse
 		arrow_position_i = get_viewport().get_mouse_position()
-		$bownarrow.show()#mostra o arco
+		show_bow()#mostra o arco
+	
 	if Input.is_action_just_released("click_left"):#guarda a posicao de quando solta o mouse
 		arrow_position_f = get_viewport().get_mouse_position()
-		$bownarrow.hide()#o arco desaaparece
+		hide_bow()#o arco desaaparece
 		
 		if can_shoot:#se pode atirar
-			spawn_arrow()#arrow goes woosh
-			can_shoot = false#n pode mais atirar ate
-			$arrow_delay.start()#o timer terminar
+			shoot_arrow()
 
-
-
-func _update():#chamado todo frame
-	if !mousehold:#se o player nao esta segurando o botao do mouse
-		play_animation(_velocity)#a animacao toca de acordo com a velocidade do player
-		$Camera2D/ScreenShake/Frequency.stop()#a tela nao treme
-	else:#se o player esta segurando o botao do mouse
-		$Camera2D/ScreenShake.start()#a tela treme
-		$AnimatedSprite.play("populo")#a animacao populo toca
-	$bownarrow.rotation = (arrow_position_i - get_viewport().get_mouse_position()).angle()#roda o arco pro lugar certo
-
+#***
 func _process(delta: float) -> void:
 	_update()
+#***
 
-func spawn_arrow():#woosh the arrow
+#woosh the arrow
+func spawn_arrow():
 	add_child(Arrow_Projectile.instance())
 	$Arrow._set_pos(global_position)
 	$Arrow.launch_arrow(arrow_position_i - arrow_position_f)#chama launch arrow do outro script
 
-func play_animation(velocity: Vector2) -> void:#toca animacao
+func take_damage():
+	if is_winged:#perde as asas
+			is_winged = false
+			extra_jump = false
+			$wings.hide()
+	elif is_shielded:#ou perde o escudo
+		is_shielded = false
+		$bubble.hide()
+	else:#ou morre
+		KillPlayer()
+
+#toca animacao
+func play_animation(velocity: Vector2) -> void:
 	#bow *********************
 	if can_shoot:#se pode atirar, o arco fica com outline
 		$bownarrow.frame = 0
@@ -125,48 +167,38 @@ func play_animation(velocity: Vector2) -> void:#toca animacao
 				$AnimatedSprite.play("jump_1")
 			else: $AnimatedSprite.play("jump_0")
 
-
-func _on_EnemyDetector_body_entered(body: Node) -> void:#quando o detector de inimigo intersecta com um corpo (marcado como inimigo)
-	if !imune:#se nao estiver estiver imune
-		if is_winged:#perde as asas
-			is_winged = false
-			extra_jump = false
-			$wings.hide()
-		elif is_shielded:#ou perde o escudo
-			is_shielded = false
-			$bubble.hide()
-		else:#ou morre
-			KillPlayer()
-		imune = true#se ele ainda nao morreu, depois de levar dano, torna-se imune por um tempo
-		$dmg_delay.start()#comeca o timer pra deixar de ser imune
-
-func _on_pickuper_area_entered(area: Area2D) -> void:#pick power ups and interact with some areas
-	if area.name == "pickup_shield":#se entrar na area do shield, pega o shield
-		is_shielded = true
-		$bubble.show()
-	if area.name == "pickup_wing":#se entrar na area da wing, pega a wing
-		is_winged = true
-		extra_jump = true
-		$wings.show()
-	if area.name == "sticky_wall_area":#se entrar na area da wall, ele gruda e vai caindo devagar
-		_velocity *= 0
-		enable_mov = true
-		gravitytemp *= 0.06# gravitytemp eh uma var temporaria, assim que ele sai dessa area gravitytemp recebe gravity (const)
-
-func _on_pickuper_area_exited(area: Area2D) -> void:#saindo da area (sticky_wall) a gravidade volta ao normal
-	gravitytemp = gravity
-
-func _on_arrow_delay_timeout() -> void:#no fim do time, o player pode atirar novamente
-	can_shoot = true
-
-
-func KillPlayer() -> void:#mata o player
+#mata o player
+func KillPlayer() -> void:
 		PlayerData.deaths += 1
 		queue_free()
 
+func show_bow():
+	$bownarrow.show()#mostra o arco
 
-func _on_dmg_delay_timeout() -> void:#no fim da invencibilidade temporaria, o player pode levar dano novamente
-	imune = false
+func hide_bow():
+	$bownarrow.hide()
 
+func shoot_arrow():
+	spawn_arrow()#arrow goes woosh
+	can_shoot = false#n pode mais atirar ate
+	$arrow_delay.start()#o timer terminar
 
+func shake_screen():
+	$Camera2D/ScreenShake.start()
 
+func shake_stop():
+	$Camera2D/ScreenShake/Frequency.stop()
+
+func pickup_shield():
+	is_shielded = true
+	$bubble.show()
+
+func pickup_wing():
+	is_winged = true
+	extra_jump = true
+	$wings.show()
+
+func stick_to_wall():
+	_velocity *= 0
+	enable_mov = true
+	gravitytemp *= 0.06
